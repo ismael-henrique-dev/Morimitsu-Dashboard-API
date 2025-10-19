e908f367492bfed35d09a3d3106a58eb7eaaaff1
Ismael <ismael.henrique.dev@gmail.com>
2025-10-18 22:02:47 -0300

    feat(login): improve login error messages

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -1,8 +1,8 @@
-import z from 'zod'
 import { prisma } from '../../lib'
-import bcrypt from 'bcrypt'
 import { Request, Response } from 'express'
 import { signToken } from '../../lib/jwt'
+import z from 'zod'
+import bcrypt from 'bcrypt'
 
 export const loginSchema = z.object({
   email: z.string().email(),
@@ -11,25 +11,45 @@ export const loginSchema = z.object({
 
 export const login = async (req: Request, res: Response) => {
   try {
-    const parsed = loginSchema.parse(req.body)
-    const { email, password } = parsed
+    const { email, password } = loginSchema.parse(req.body)
 
     const user = await prisma.users.findUnique({ where: { email } })
-    if (!user) return res.status(401).json({ message: 'Credenciais inválidas' })
 
-    const match = await bcrypt.compare(password, user.password)
-    if (!match)
-      return res.status(401).json({ message: 'Credenciais inválidas' })
+    if (!user)
+      return res.status(401).json({ message: 'Email or password incorrects' })
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid)
+      return res.status(401).json({ message: 'Email or password incorrects' })
 
     const token = signToken({ userId: user.id, role: user.role })
+
+    const userData = {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    }
+
     res.json({
       token,
+      data: userData,
     })
-  } catch (err) {
-    if (err instanceof z.ZodError) {
-      return res.status(400).json({ errors: err.message })
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      const errors = error.issues.map((issue) => ({
+        field: issue.path[0],
+        message: issue.message,
+      }))
+
+      return res.status(400).json({
+        message: 'Validation error',
+        errors,
+      })
     }
-    console.error(err)
-    res.status(500).json({ message: 'Erro no servidor', error: err })
+
+    console.error('[LOGIN_ERROR]', error)
+    return res.status(500).json({ message: 'Internal server error' })
   }
 }
