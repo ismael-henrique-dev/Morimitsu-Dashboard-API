0d82610a3b720c210eec3dcfaa4181ec48a3e5d6
RaiRamos2 <rairamos674@gmail.com>
2025-10-27 22:54:26 -0300

    feat: atualizatin classes

--- src/repositories/classes.ts
+++ src/repositories/classes.ts
@@ -4,6 +4,9 @@ import { prisma } from '../lib'
 export interface ClassesRepositoryInterface {
   create(data: Prisma.classesUncheckedCreateInput): Promise<classes>
   delete(classId: string): Promise<void>
+  update(classId: string, data: Partial<Prisma.classesUncheckedUpdateInput>): Promise<classes>
+  search(classId: string) : Promise<classes | null>
+  read (): Promise<classes[]>
 }
 
 //No prisma
@@ -19,7 +22,22 @@ export class PrismaClassesRepository implements ClassesRepositoryInterface {
       where: { id: classId },
     })
   }
+  async update(classId: string, data: Partial<Prisma.classesUncheckedUpdateInput>): Promise<classes> {
+    return await prisma.classes.update({
+      where: { id: classId },
+      data,
+    })
+  }
+  async search(classId: string): Promise<classes | null> {
+    return await prisma.classes.findUnique({
+      where: { id: classId },
+    })
+  }
+  async read(): Promise<classes[]> {
+    return await prisma.classes.findMany()
+  }
 }
+
 
 //Em memória
 export class InMemoryClassesRepository implements ClassesRepositoryInterface {
@@ -40,5 +58,22 @@ export class InMemoryClassesRepository implements ClassesRepositoryInterface {
 
   async delete(classId: string): Promise<void> {
     this.list = this.list.filter((c) => c.id !== classId)
+  }
+
+  async update(classId: string, data: Partial<Prisma.classesUncheckedUpdateInput>): Promise<classes> {
+    const idx = this.list.findIndex((c) => c.id === classId)
+    if (idx === -1) throw new Error('Turma não encontrada')
+
+    const updatedClass = { ...this.list[idx], ...data } as classes
+    this.list[idx] = updatedClass
+    return updatedClass
+  }
+
+  async search(classId: string): Promise<classes | null> {
+    const c = this.list.find((c) => c.id === classId)
+    return c || null
+  }
+  async read(): Promise<classes[]> {
+    return this.list
   }
 }
--- src/routes/classes.ts
+++ src/routes/classes.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express'
 import { createClassController } from '../controllers/classes/create'
 import { deleteClassController } from '../controllers/classes/delete'
+import { updateClassController } from '../controllers/classes/update'
+import { searchClassController } from '../controllers/classes/search'
+import { readClassController } from '../controllers/classes/read'
 import { authenticate } from '../middlewares/auth'
 
 const router = Router()
 
 router.post('/create', authenticate, createClassController)
 router.delete('/delete/:id', authenticate, deleteClassController)
-
+router.patch('/update/:id', authenticate, updateClassController)
+router.get('/search/:id', authenticate, searchClassController)
+router.get('/read', authenticate, readClassController)
 
 export default router
